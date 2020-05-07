bfe9f7edffc62955da878d9a6b7c7806c75552c6
MinaMaher0 <minamaher30697@gmail.com>
2020-05-07 04:54:08 +0200

    add circleci

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@
 dockerpath=948036832276.dkr.ecr.us-east-2.amazonaws.com/udacity:latest
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run app2 --image=948036832276.dkr.ecr.us-east-2.amazonaws.com/udacity:latest --port=80
+kubectl run app --image=948036832276.dkr.ecr.us-east-2.amazonaws.com/udacity:latest --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -16,4 +16,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment app2 --type=LoadBalancer --port=8000 --target-port=80
+kubectl expose deployment app --type=LoadBalancer --port=8000 --target-port=80
