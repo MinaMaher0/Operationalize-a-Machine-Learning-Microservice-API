9ef8370d134c517ab89fca073cda25e7ab5ee3ef
MinaMaher0 <minamaher30697@gmail.com>
2020-05-07 02:09:45 +0200

    add log to app

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ RUN pip install --upgrade pip && pip install -r requirements.txt
 
 EXPOSE 80
 
-CMD ["pyhton", "app.py"]
+CMD ["python", "app.py"]
--- run_docker.sh
+++ run_docker.sh
@@ -2,15 +2,9 @@
 
 ## Complete the following steps to get Docker running locally
 
-# Step 1:
-# Build image and add a descriptive tag
-docker build --tag=App .
+docker build --tag=app .
 
-# Step 2: 
-# List docker images
 docker images ls
 
-# Step 3: 
-# Run flask app
-docker run -p 80:80 App app
+docker run -p 8000:80 app
 
