d092e113021c09357ea67dd4e8bff42205240cd5
MinaMaher0 <minamaher30697@gmail.com>
2020-05-07 02:30:07 +0200

    upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,10 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=app
 
-# Step 2:  
-# Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 948036832276.dkr.ecr.us-east-2.amazonaws.com
+docker tag $dockerpath 948036832276.dkr.ecr.us-east-2.amazonaws.com/udacity:latest
 
-# Step 3:
-# Push image to a docker repository
+docker push 948036832276.dkr.ecr.us-east-2.amazonaws.com/udacity:latest
